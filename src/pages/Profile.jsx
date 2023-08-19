82858b5340e72ae13aff4b4f3ec0caa8f8803243
LemonDrop847 <nitinmishra847@gmail.com>
2023-08-19 19:56:49 +0530

    Added Coin

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,69 @@
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from "../services/firebase";
+import coin from "../assets/coin.png";
 export default function Profile() {
-  return <div>Profile</div>;
+  const [userData, setUserData] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const userId = auth.currentUser.uid;
+
+  useEffect(() => {
+    const userRef = doc(db, "seller", userId);
+    getDoc(userRef)
+      .then((docSnapshot) => {
+        if (docSnapshot.exists()) {
+          setUserData(docSnapshot.data());
+          fetchBalance(docSnapshot.data().walletAddress);
+        } else {
+          console.log("User not found.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+  }, [userId]);
+
+  const fetchBalance = async (walletAddress) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/fetchBalance`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ address: walletAddress }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setBalance(data.balance);
+      } else {
+        console.error("Error fetching balance:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  };
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="border-2 bg-lime-400 rounded-md flex flex-col justify-center items-center p-8 px-20 w-1/3">
+        <h1 className="mt-10 text-4xl text-white">Profile</h1>
+        <p className="text-white">Name: {userData.name}</p>
+        <p className="text-white">Email: {userData.email}</p>
+        <p className="text-white">Type: {userData.type}</p>
+        <p className="text-white">Wallet Address: {userData.walletAddress}</p>
+        <p className="text-white">
+          <img src={coin} height="50px" width="50px" />
+          GCT Balance: {balance}
+        </p>
+      </div>
+    </div>
+  );
 }
