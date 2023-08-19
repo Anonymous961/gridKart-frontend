5b2fbb8220d5c0d3f5710ab07d5768e9a0b9074d
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-08-20 00:21:58 +0530

    Revert "Merge pull request #2 from LemonDrop847/almost-complete"
    This reverts commit 7262bd54c51987b8c33813c36cd6721532de50e8, reversing
    changes made to 2d01e1b600dee2fcaebd27e92761bcedbad9d327.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./services/firebase";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
 import Profile from "./pages/Profile";
-import AddProduct from "./pages/AddEdit";
-import Redeem from "./pages/Redeem";
-import Success from "./pages/SuccessRedeem";
-import SuccessBuy from "./pages/SuccessBuy";
-import SuccessRecived from "./pages/SuccessRecieved";
-import BuyPage from "./pages/BuyPage";
 import History from "./pages/History";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        user.displayName === "seller" ? setUser("allowed") : setUser(null);
-      } else {
-        setUser(null);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
   return (
     <Router>
       <Navbar />
@@ -39,12 +17,6 @@ function App() {
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/redeem" element={<Redeem />} />
-        <Route exact path="/buy" element={<BuyPage />} />
-        <Route exact path="/redeem/success" element={<Success />} />
-        <Route exact path="/buy/success" element={<SuccessBuy />} />
-        <Route exact path="/product/success" element={<SuccessRecived />} />
-        {user && <Route path="/addProducts" element={<AddProduct />} />}
         <Route exact path="/history" element={<History />} />
       </Routes>
       <Footer />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,67 +1,5 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { doc, setDoc } from "firebase/firestore";
-import { db, auth } from "../services/firebase";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-
 export default function Signup() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [type, setType] = useState("user");
-  const [password, setPassword] = useState("");
-  const [walletAddress, setWalletAddress] = useState("");
-  const [isWalletConnected, setIsWalletConnected] = useState(false);
-
-  const connectWallet = async (e) => {
-    e.preventDefault();
-    try {
-      if (window.ethereum) {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-        setIsWalletConnected(true);
-      } else {
-        console.error("MetaMask is not installed.");
-      }
-    } catch (error) {
-      console.error("Error connecting to MetaMask:", error);
-    }
-  };
-  const handleSignup = async (e) => {
-    e.preventDefault();
-
-    try {
-      if (!isWalletConnected) {
-        console.error("Connect your MetaMask wallet first.");
-        return;
-      }
-
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-      const userRef = doc(db, type, user.uid);
-      await setDoc(userRef, {
-        name: name,
-        email: email,
-        type: type,
-        walletAddress: walletAddress,
-      });
-      await updateProfile(user, {
-        displayName: type,
-      });
-      console.log("Signed Up!");
-    } catch (error) {
-      console.error("Error signing up:", error);
-    }
-  };
-  const handleCheckboxChange = (e) => {
-    setType(e.target.checked ? "seller" : "user");
-  };
-
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="border-2 bg-lime-400 rounded-md flex flex-col justify-center items-center p-8 px-20 w-1/3">
@@ -71,50 +9,28 @@ export default function Signup() {
             type="text"
             className="p-2 rounded-md m-1 w-3/4"
             placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type=""
+            className="p-2 rounded-md m-1 w-3/4"
+            name=""
+            placeholder="Mobile number"
+            id=""
           />
           <input
             className="p-2 rounded-md m-1 w-3/4"
             type="email"
             placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="p-2 rounded-md m-1 w-3/4"
             type="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
-          <label className="flex items-center m-1">
-            Are you a seller?
-            <input
-              type="checkbox"
-              className="ml-2"
-              checked={type === "seller"}
-              onChange={handleCheckboxChange}
-            />
-          </label>
-          <button
-            onClick={connectWallet}
-            className="m-3 bg-blue-400 p-3 rounded-md"
-          >
-            Connect MetaMask Wallet
-          </button>
-          <button
-            onClick={handleSignup}
-            className={`m-3 bg-green-400 p-3 rounded-md ${
-              !isWalletConnected && "cursor-not-allowed opacity-50"
-            }`}
-            disabled={!isWalletConnected}
-          >
-            Sign Up
-          </button>
+          <input type="submit" className="m-3 bg-green-400 p-3 rounded-md" />
         </form>
         <p>
-          Already a user?
+          Already an user? 
           <Link to="/login" className="text-purple-500 underline">
             Login
           </Link>
